8c21cd2c9efaacaee608f5b88f78481c6d7cd8cf
Bruno "Borta" Bortagaray <khalisto@gmail.com>
2018-06-04 20:18:12 -0300

    Update drop.sql

--- drop.sql
+++ drop.sql
@@ -6,3 +6,10 @@ drop table aluno;
 drop table disciplina;
 drop table curso;
 drop table area;
+
+drop sequence area_SEQ;
+drop sequence curso_SEQ;
+drop sequence disciplina_SEQ;
+drop sequence aluno_SEQ;
+drop sequence professor_SEQ;
+drop sequence turma_SEQ;
